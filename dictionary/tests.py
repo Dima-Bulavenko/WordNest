dcbe876314a6fd9f950ced142d553c4a0aa3ab28
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-05-27 16:48:48 +0100

    test: Create "test_create_user" for testing "UserManager" class.

--- dictionary/tests.py
+++ dictionary/tests.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class UserManagerTest(TestCase):
+    def setUp(self):
+        self.user_model = get_user_model()
+        self.test_email = "testeamil@test.com"
+        self.test_pwd = "testpassword"
+
+    def test_create_user(self):
+        user = self.user_model.objects.create_user(email=self.test_email,
+                                             password=self.test_pwd)
+        user_exist = self.user_model.objects.filter(email=self.test_email).exists()
+
+        self.assertEqual(user.email, self.test_email)
+        self.assertTrue(user_exist)
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+        self.assertTrue(user.check_password(self.test_pwd))
+
+
+
+
