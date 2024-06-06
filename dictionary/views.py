ef3bb78d86f6053b155865461f52707526ad28a6
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-06-06 21:00:08 +0100

    fix: Resolve "BUG: Title of the "index.html" template doesn't change imediately" closes #13

--- dictionary/views.py
+++ dictionary/views.py
@@ -5,12 +5,21 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = Path("dictionary", "index.html")
-    extra_context = {"title": "WordNest Online Dictionary"}
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        
+        if self.request.user.is_authenticated:
+            title = "WordNest Online Dictionary"
+        else:
+            title = "Welcome"
+
+        context_data["title"] = title
+        return context_data
 
     def get_template_names(self):
         if not self.request.user.is_authenticated:
             result = [Path("welcome.html")]
-            self.extra_context["title"] = "Welcome"
         else:
             result = super().get_template_names()
 
