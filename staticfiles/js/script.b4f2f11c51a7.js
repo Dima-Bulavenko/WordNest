310fbb84b23a3e4b4a0734297a2000fd14a485cc
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-06-15 00:24:41 +0100

    feat: Static files created by "py manage.py collectstatic" command.

---
+++ staticfiles/js/script.b4f2f11c51a7.js
@@ -0,0 +1,21 @@
+// Configuration
+const burgerMenuSelector = "#burger-menu";
+const navigationSelector = "#navigation";
+
+/**
+ * Toggles the 'active' class for each given element.
+ *
+ * @param {...Element} elements - The elements to toggle the 'active' class for.
+ */
+function toggleActiveClass(...elements) {
+    elements.forEach((element) => element.classList.toggle("active"));
+}
+
+// Event listeners
+let burgerMenu = document.querySelector(burgerMenuSelector);
+
+burgerMenu.addEventListener("click", function () {
+    toggleActiveClass(this, document.querySelector(navigationSelector));
+});
+
+module.exports = { toggleActiveClass };
