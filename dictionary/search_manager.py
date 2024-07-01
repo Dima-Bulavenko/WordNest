2c644e6a7eef2462c6d52565bce8134c7fa66368
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-07-01 13:56:22 +0100

    feat: Create "TranslationAPI" to translate text and words.
    TranlationAPI instance: has this attributes:
    client - API clients with methods to retrive data.
    from_language - language to translate from.
    to_language - language to translate to.
    body - text or word for translation.
    
    Methods:
    
    translate(): attempts to translate the text by looking up dictionary.
    If no dictionary entries, it falls back to translating the text.
    
    translate_text(): translate text and return templated data.
    
    lookup_dictionary_entries(): translate word and return tempated data.
    
    get_templated_data(data): format data to common tamplate.
    
    has_translation(data): check that data has "translations" key.

---
+++ dictionary/search_manager.py
@@ -0,0 +1,134 @@
+from typing import Union
+
+from azure.ai.translation.text import TextTranslationClient
+from azure.ai.translation.text.models import DictionaryLookupItem, TranslatedTextItem
+from azure.core.credentials import AzureKeyCredential
+from azure.core.exceptions import HttpResponseError
+from decouple import config
+
+
+class TranslationAPI:
+    def __init__(self, *, from_language: str, to_language: str, body: str):
+        self.__key = AzureKeyCredential(config("AZURE_TRANSLATOR_KEY"))
+        self.client = TextTranslationClient(credential=self.__key)
+        self.from_language = from_language
+        self.to_language = to_language
+        self.body = body
+
+    def translate(self) -> dict:
+        """
+        Translates text by looking up dictionary entries and translating text.
+
+        This method first attempts to translate the text by looking up dictionary entries.
+        If no dictionary entries are found, it falls back to translating the text.
+
+        Returns:
+            dict: The translated text as a dictionary.
+        """
+        data = self.lookup_dictionary_entries()
+        if data:
+            return data
+
+        data = self.translate_text()
+        return data
+
+    def translate_text(self) -> dict:
+        """
+        Translates text using a client service.
+
+        Send a request to the client service to translate the text from 
+        the source language to the target language.
+
+        Returns:
+            dict: The translated text as a dictionary.
+        """
+        data = self.client.translate(
+            body=[self.body],
+            from_language=self.from_language,
+            to_language=[self.to_language],
+        )[0]
+
+        return self.get_templated_data(data)
+
+    def lookup_dictionary_entries(self) -> DictionaryLookupItem:
+        """
+        Looks up dictionary entries for the text.
+
+        Send a request to the client service to look up dictionary entries
+        for the text from the source language to the target language.
+
+        Returns:
+            DictionaryLookupItem: The dictionary entries for the text as a DictionaryLookupItem.
+        """
+        try:
+            data = self.client.lookup_dictionary_entries(
+                body=[self.body],
+                from_language=self.from_language,
+                to_language=self.to_language,
+            )[0]
+        except HttpResponseError as ex:
+            if ex.error.code == 400050:  # text exceeds the maximum length of 100
+                return None
+            raise
+
+        if self.has_translation(data):
+            templated_data = self.get_templated_data(data)
+            return templated_data
+        return None
+
+    def get_templated_data(
+        self, data: Union[DictionaryLookupItem, TranslatedTextItem]
+    ) -> dict:
+        """
+        Formats the translation data into a specific template.
+
+        Takes in a data object, which can be either a DictionaryLookupItem or a
+        TranslatedTextItem, and formats the translation data into a specific template.
+
+        Parameters:
+            data (Union[DictionaryLookupItem, TranslatedTextItem]): The data object containing
+            the translation information.
+
+        Returns:
+            dict: A dictionary containing the formatted translation data.
+        """
+        templated_data = {
+            "form_lang": self.from_language,
+            "to_lang": self.to_language,
+            "word": self.body,
+            "translations": [],
+        }
+
+        for trans_num, translation in enumerate(data.translations, 1):
+            if trans_num > 3:
+                break
+
+            if isinstance(data, TranslatedTextItem):
+                text = translation.text
+                pos = ""
+                prefix_word = ""
+            else:
+                text = translation.normalized_target
+                pos = translation.pos_tag
+                prefix_word = translation.prefix_word
+
+            translation_data = {
+                "text": text,
+                "pos": pos,
+                "prefix_word": prefix_word,
+            }
+            templated_data["translations"].append(translation_data)
+        return templated_data
+
+    def has_translation(self, data: dict) -> bool:
+        """
+        Checks if the provided data contains any translations.
+
+        Parameters:
+            data (dict): The data object containing the translation information.
+
+        Returns:
+            bool: True if the 'translations' key is present and has a truthy value, False otherwise.
+        """
+        key = "translations"
+        return key in data and bool(data[key])
