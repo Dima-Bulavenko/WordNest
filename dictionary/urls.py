0a44bbcbf1609f6dba708818df312fb7e8d66d9a
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-07-01 14:25:34 +0100

    feat: Create "TranslationView" to process AJAX requests.
    urls.py: Add urlpattern for translate request.
    
    views.py: Create "TranslationView" class. The class serve post-ajax
    requests and return JsonResponse or page not found error.

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -4,4 +4,5 @@ from dictionary import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="home"),
+    path("translate/", views.TranslationView.as_view(), name="translation")
 ]
--- dictionary/views.py
+++ dictionary/views.py
@@ -1,6 +1,11 @@
+import json
 from pathlib import Path
 
+from django.http import JsonResponse, Http404
+from django.views import View
 from django.views.generic import TemplateView
+
+from dictionary.search_manager import TranslationAPI
 
 
 class IndexView(TemplateView):
@@ -8,7 +13,7 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
-        
+
         if self.request.user.is_authenticated:
             title = "WordNest Online Dictionary"
         else:
@@ -24,3 +29,22 @@ class IndexView(TemplateView):
             result = super().get_template_names()
 
         return result
+
+
+class TranslationView(View):
+    def post(self, request, *args, **kwargs):
+        if self.is_ajax():
+            data = json.loads(request.body.decode("utf-8"))
+            client = TranslationAPI(**data)
+            translation = client.translate()
+            return JsonResponse(translation)
+        raise Http404("Page not fount")
+
+    def is_ajax(self) -> bool:
+        """
+        Checks if the request was made via AJAX.
+
+        Returns:
+            bool: True if the request was made via AJAX, False otherwise.
+        """
+        return self.request.headers.get("X-Requested-With") == "XMLHttpRequest"
