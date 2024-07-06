32627aebef294f34768f931b0524edfd156d2e00
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-07-06 20:50:19 +0100

    test: Create tests for "Language" model.

--- dictionary/tests/test_models.py
+++ dictionary/tests/test_models.py
@@ -24,4 +24,30 @@ class WordTest(TestCase):
     def test_str_method(self):
         word = Word.objects.create(word="hello", language=self.language)
         self.assertEqual(str(word), "hello")
+    
 
+class LanguageTest(TestCase):
+    def test_create_language(self):
+        language = Language.objects.create(code="en", name="English")
+        
+        self.assertEqual(language.code, "en")
+        self.assertEqual(language.name, "English")
+    
+    def test_unique_code_constraint(self):
+        code = 'en'
+        Language.objects.create(code=code, name="English")
+        
+        with self.assertRaises(IntegrityError):
+            Language.objects.create(code=code, name="Ukrainian")
+    
+    def test_unique_name_constraint(self):
+        name = 'English'
+        Language.objects.create(code="en", name=name)
+        
+        with self.assertRaises(IntegrityError):
+            Language.objects.create(code="uk", name=name)
+    
+    def test_str_method(self):
+        name = "English"
+        language = Language.objects.create(code="en", name=name)
+        self.assertEqual(str(language), name)
