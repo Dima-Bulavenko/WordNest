75ed46c9a92c974bc1019400a10b80911b22670e
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-07-06 22:23:57 +0100

    test: Create tests for "Translation" model.

--- dictionary/tests/test_models.py
+++ dictionary/tests/test_models.py
@@ -1,4 +1,5 @@
 from django.db.utils import IntegrityError
+from django.forms import ValidationError
 from django.test import TestCase
 
 from dictionary.models import Dictionary, Language, Translation, User, Word
@@ -51,3 +52,34 @@ class LanguageTest(TestCase):
         name = "English"
         language = Language.objects.create(code="en", name=name)
         self.assertEqual(str(language), name)
+
+
+class TranslationTest(TestCase):
+    def setUp(self):
+        self.language1 = Language.objects.create(code="en", name="English")
+        self.language2 = Language.objects.create(code="uk", name="Ukrainian")
+        self.word1 = Word.objects.create(word="hello", language=self.language1)
+        self.word2 = Word.objects.create(word="привіт", language=self.language2)
+    
+    def test_create_translation(self):
+        Translation.objects.create(from_word=self.word1, to_word=self.word2)
+
+        translations = Translation.objects.all()
+        
+        self.assertEqual(translations.count(), 1)
+        self.assertTrue(self.word2 == translations[0].to_word)
+        self.assertTrue(self.word1 == translations[0].from_word)
+    
+    def test_unique_translation_pair_constraint(self):
+        Translation.objects.create(from_word=self.word1, to_word=self.word2)
+
+        with self.assertRaises(IntegrityError):
+            Translation.objects.create(from_word=self.word1, to_word=self.word2)
+    
+    def test_save_method(self):
+        with self.assertRaises(ValidationError):
+            Translation.objects.create(from_word=self.word1, to_word=self.word1)
+    
+    def test_str_method(self):
+        translation = Translation.objects.create(from_word=self.word1, to_word=self.word2)
+        self.assertEqual(str(translation), "hello -> привіт")
