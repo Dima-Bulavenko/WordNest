e7ac4c056670ece6969415f9d9fc1e70c2a6cf6a
Dima Bulavenko <dima99bylovenko@gmail.com>
2024-07-09 17:44:30 +0100

    feat: Create "DictionaryForm" model form to implement dictionary creation.

--- dictionary/forms.py
+++ dictionary/forms.py
@@ -1,6 +1,9 @@
 from allauth.account.forms import LoginForm
+from django import forms
 from django.urls import NoReverseMatch, reverse
 from django.utils.safestring import mark_safe
+
+from dictionary.models import Dictionary
 
 
 class LoginForm(LoginForm):
@@ -19,3 +22,14 @@ class LoginForm(LoginForm):
             return mark_safe(
                 f'<div class="forgot_pwd align-center auth-link"><a href="{reset_url}">{forgot_txt}</a></div>'
             )
+
+
+class DictionaryForm(forms.ModelForm):
+    class Meta:
+        select_attrs = {"class": "select_language"}
+        model = Dictionary
+        fields = ["source_language", "target_language"]
+        widgets = {
+            "source_language": forms.Select(attrs=select_attrs),
+            "target_language": forms.Select(attrs=select_attrs),
+        }
